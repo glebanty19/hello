34d87e93af3e899b676f09f5016eae18a433e91b
glebanty19 <167649626+glebanty19@users.noreply.github.com>
2024-04-24 13:55:21 +0300

    Знайти довжину дуги кола, якщо відомі радіус кола та центральний кут, який вона охоплює.

---
+++ labbb3/labbb3/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace labbb3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Отримання значень з текстових полів
+                double radius = double.Parse(textBox1.Text);
+                double centralAngle = double.Parse(textBox2.Text);
+
+                // Обчислення довжини дуги
+                double arcLength = (centralAngle / 360) * 2 * Math.PI * radius;
+
+                // Виведення результату
+                textBox3.Text = arcLength.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Будь ласка, введіть коректні значення для радіуса та кута.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка: {ex.Message}");
+            }
+        }
+    }
+}
