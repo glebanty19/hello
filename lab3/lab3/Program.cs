4c3a6bb60353452093c8ae5f4d63d3342af0e690
Hleb Bondar <hlebbondar2004@gmail.com>
2025-01-26 20:11:16 +0200

    add space

--- lab3/lab3/Program.cs
+++ lab3/lab3/Program.cs
@@ -5,10 +5,10 @@ class Program
     static void Main(string[] args)
     {
         // Введення радіусу та центрального кута у градусах
-        Console.WriteLine("Введіть радіус кола:");
+        Console.WriteLine("Введіть радіус кола: ");
         double radius = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("Введіть центральний кут у градусах:");
+        Console.WriteLine("Введіть центральний кут у градусах: ");
         double centralAngle = Convert.ToDouble(Console.ReadLine());
 
         // Переведення кута в радіани
